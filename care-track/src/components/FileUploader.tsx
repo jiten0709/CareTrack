c8c2c9aa8acbb2f3566864845bd8058456ec5541
jiten0709 <parmar.jiten.microsoft@gmail.com>
2025-01-26 11:55:00 +0530

    add FileUploader component; enhance formatDateTime function to accept timeZone parameter (on-going)

---
+++ care-track/src/components/FileUploader.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+import Image from "next/image";
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
--- care-track/src/lib/utils.ts
+++ care-track/src/lib/utils.ts
@@ -1,72 +1,76 @@
 import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const parseStringify = (value: any) => JSON.parse(JSON.stringify(value));
+export const parseStringify = (value: any) => JSON.parse(JSON.stringify(value))
 
-export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
+export const convertFileToUrl = (file: File) => URL.createObjectURL(file)
 
-// FORMAT DATE TIME
-export const formatDateTime = (dateString: Date | string) => {
+// format date time
+export const formatDateTime = (dateString: Date | string, timeZone: string = Intl.DateTimeFormat().resolvedOptions().timeZone) => {
   const dateTimeOptions: Intl.DateTimeFormatOptions = {
-    // weekday: "short", // abbreviated weekday name (e.g., 'Mon')
-    month: "short", // abbreviated month name (e.g., 'Oct')
-    day: "numeric", // numeric day of the month (e.g., '25')
-    year: "numeric", // numeric year (e.g., '2023')
-    hour: "numeric", // numeric hour (e.g., '8')
-    minute: "numeric", // numeric minute (e.g., '30')
-    hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
-  };
+    weekday: "short",
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+    timeZone: timeZone,
+  }
 
   const dateDayOptions: Intl.DateTimeFormatOptions = {
-    weekday: "short", // abbreviated weekday name (e.g., 'Mon')
-    year: "numeric", // numeric year (e.g., '2023')
-    month: "2-digit", // abbreviated month name (e.g., 'Oct')
-    day: "2-digit", // numeric day of the month (e.g., '25')
-  };
+    weekday: "short",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    timeZone: timeZone,
+  }
 
   const dateOptions: Intl.DateTimeFormatOptions = {
-    month: "short", // abbreviated month name (e.g., 'Oct')
-    year: "numeric", // numeric year (e.g., '2023')
-    day: "numeric", // numeric day of the month (e.g., '25')
-  };
+    month: "short",
+    year: "numeric",
+    day: "numeric",
+    timeZone: timeZone,
+  }
 
   const timeOptions: Intl.DateTimeFormatOptions = {
-    hour: "numeric", // numeric hour (e.g., '8')
-    minute: "numeric", // numeric minute (e.g., '30')
-    hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
-  };
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+    timeZone: timeZone,
+  }
 
   const formattedDateTime: string = new Date(dateString).toLocaleString(
     "en-US",
     dateTimeOptions
-  );
+  )
 
   const formattedDateDay: string = new Date(dateString).toLocaleString(
     "en-US",
     dateDayOptions
-  );
+  )
 
   const formattedDate: string = new Date(dateString).toLocaleString(
     "en-US",
     dateOptions
-  );
+  )
 
   const formattedTime: string = new Date(dateString).toLocaleString(
     "en-US",
-    timeOptions
-  );
+    timeOptions,
+  )
 
   return {
     dateTime: formattedDateTime,
     dateDay: formattedDateDay,
     dateOnly: formattedDate,
     timeOnly: formattedTime,
-  };
-};
+  }
+}
 
 export function encryptKey(passkey: string) {
   return btoa(passkey);
